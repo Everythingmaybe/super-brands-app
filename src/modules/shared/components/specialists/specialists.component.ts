c4161877e227a151887cc94e464e482c34bd366d
Everythingmaybe <55mixxx55@gmail.com>
2020-05-31 07:37:25 +0600

    Install lodash. Change adding/removing methods.

--- src/modules/shared/components/specialists/specialists.component.ts
+++ src/modules/shared/components/specialists/specialists.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import * as lodash from 'lodash';
+
 import { Shop, Specialist, SpecialistsService } from './specialists.service';
 
 @Component({
   selector: 'app-specialists',
   templateUrl: './specialists.component.html',
-  styleUrls: ['./specialists.component.scss']
+  styleUrls: ['./specialists.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpecialistsComponent implements OnInit {
 
@@ -42,37 +45,44 @@ export class SpecialistsComponent implements OnInit {
   }
 
   // Todo: Необходимо реализовать хранилище данных и перенести всю логику добавления/удаления туда.
-
   public addShopToSpecialist(shopId: number): void {
-    const shop = this.shops.find((s) => s.id === shopId);
+    const shops = lodash.cloneDeep(this.shops);
+    const shop = shops.find((s) => s.id === shopId);
     shop.specialistId = this.selectedSpecialist.id;
 
-    const specialist = this.specialists.find((s) => s.id === this.selectedSpecialist.id);
+    const specialists = lodash.cloneDeep(this.specialists);
+    const specialist = specialists.find((s) => s.id === this.selectedSpecialist.id);
     specialist.shopIds = [...specialist.shopIds, shopId];
 
-    this.shops = [ ...this.shops ];
-    this.specialists = [ ...this.specialists ];
+    this.shops = shops;
+    this.specialists = specialists;
   }
 
   public removeSpecialist(specialistId: number): void {
-    const specialist = this.specialists.find((s) => s.id === specialistId);
+    const specialists = lodash.cloneDeep(this.specialists);
+    const shops = lodash.cloneDeep(this.shops);
+    const specialist = specialists.find((s) => s.id === specialistId);
+
     specialist.shopIds.forEach((shopId) => {
-      const shop = this.shops.find((s) => s.id === shopId);
+      const shop = shops.find((s) => s.id === shopId);
       shop.specialistId = null;
     });
-    this.specialists = this.specialists.filter((s) => s.id !== specialistId);
-    this.shops = [ ...this.shops ];
+    this.specialists = specialists.filter((s) => s.id !== specialistId);
+    this.shops = shops;
     this.selectedSpecialist = this.specialists[0] || null;
   }
 
   public removeShopFromSpecialist(shopId: number, specialistId: number) {
-    const specialist = this.specialists.find((s) => s.id === specialistId);
+    const specialists = lodash.cloneDeep(this.specialists);
+    const shops = lodash.cloneDeep(this.shops);
+    const specialist = specialists.find((s) => s.id === specialistId);
+
     specialist.shopIds = specialist.shopIds.filter((_shopId) => _shopId !== shopId);
-    const shop = this.shops.find((s) => s.id === shopId);
+    const shop = shops.find((s) => s.id === shopId);
     shop.specialistId = null;
 
-    this.shops = [ ...this.shops ];
-    this.specialists = [ ...this.specialists ];
+    this.shops = shops;
+    this.specialists = specialists;
   }
 
   public trackByFn(index, item): number {
