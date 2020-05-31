0b9a111e4f2a44d00a01d8caef8714c0ba213fba
Everythingmaybe <55mixxx55@gmail.com>
2020-05-31 06:49:07 +0600

    Added i18nPlural pipe to specialists-tabs.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from '../modules/shared/shared.module';
+
+registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
